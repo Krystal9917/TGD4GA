2fbdd178ea4290f04662fa62408447b7f943082f
jiujiuchen <jiujiuchen@tencent.com>
2024-12-31 14:58:09 +0800

    commit

--- pytorch/trainer/uin_gangs_model_tuning_run.py
+++ pytorch/trainer/uin_gangs_model_tuning_run.py
@@ -87,11 +87,11 @@ class ArgsUinGangs:
         parser.add_argument('--conv_type', type=str, default='RGCN', choices=['RGCN', 'HGT'])
         parser.add_argument('--is_supervised', type=bool, default=False)
         parser.add_argument('--is_weighted_subgraph', type=bool, default=False)
-        parser.add_argument('--prompt_insertion_type', type=str, default='concat_subgraph',
+        parser.add_argument('--prompt_insertion_type', type=str, default='node_subtract_subgraph',
                             choices=[None, 'add_prompt', 'add_subgraph', 'concat_prompt', 'concat_subgraph',
                                      'concat_subgraph_prompt', 'concat_subgraph_plus_prompt',
                                      'concat_subgraph_proj_prompt', 'linear_concat_subgraph_concat_prompt',
-                                     'weighted_addition_concat_prompt'])
+                                     'weighted_addition_concat_prompt', 'node_subtract_subgraph'])
         parser.add_argument('--test_times', type=int, default=5)
 
         args = parser.parse_args()
