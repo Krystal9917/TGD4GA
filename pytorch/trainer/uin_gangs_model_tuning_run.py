3d2828726d9d97e777d51f7239af5c753cf545cc
jiujiuchen <jiujiuchen@tencent.com>
2025-01-02 17:50:54 +0800

    commit

--- pytorch/trainer/uin_gangs_model_tuning_run.py
+++ pytorch/trainer/uin_gangs_model_tuning_run.py
@@ -24,7 +24,7 @@ class ArgsUinGangs:
                          "uin_gangs_full_graph_dataset_train_241201_20241119.txt")))
         parser.add_argument('--node_test_data_path', type=str, default=os.path.abspath(
             os.path.join(os.path.dirname(__file__), os.path.pardir, os.path.pardir, "data",
-                         "uin_gangs_full_graph_dataset", "valid", "raw",
+                         "uin_gangs_full_graph_dataset", "train", "raw",
                          "uin_gangs_full_graph_dataset_train_241202_20241119.txt")))
         parser.add_argument('--prompt_initial_data_path', type=str, default=os.path.abspath(
             os.path.join(os.path.dirname(__file__), os.path.pardir, os.path.pardir, "data",
@@ -80,16 +80,16 @@ class ArgsUinGangs:
         parser.add_argument('--node_types', type=int, default=19)
         parser.add_argument('--num_relations', type=int, default=10)
         parser.add_argument('--num_heads', type=int, default=2)
-        parser.add_argument('--filter_node_num', type=int, default=3)
+        parser.add_argument('--filter_node_num', type=int, default=5)
         parser.add_argument('--sampling', type=str, default='fraudar', choices=['fraudar', 'random'])
         parser.add_argument('--drop_ratio', type=float, default=0.2)
         parser.add_argument('--is_debug', type=bool, default=False)
-        parser.add_argument('--data_tag', type=str, default='1930_', choices=['', '1921_', '1930_', 'order_1930_'])
-        parser.add_argument('--device_tag', type=str, default='', choices=['', '_GPU2'])
-        parser.add_argument('--eval_epoch', type=int, default=10)
+        parser.add_argument('--data_tag', type=str, default='order_1930_', choices=['', '1921_', '1930_', 'order_1930_'])
+        parser.add_argument('--device_tag', type=str, default='_GPU2', choices=['', '_GPU2'])
+        parser.add_argument('--eval_epoch', type=int, default=20)
         parser.add_argument('--evaluate_task', type=str, default='node_classification',
                             choices=['node_classification', 'subgraph', 'subgraph_prompt_tuning'])
-        parser.add_argument('--conv_type', type=str, default='HGT', choices=['RGCN', 'HGT'])
+        parser.add_argument('--conv_type', type=str, default='RGCN', choices=['RGCN', 'HGT'])
         parser.add_argument('--is_supervised', type=bool, default=False)
         parser.add_argument('--is_weighted_subgraph', type=bool, default=False)
         parser.add_argument('--prompt_insertion_type', type=str, default=None,
