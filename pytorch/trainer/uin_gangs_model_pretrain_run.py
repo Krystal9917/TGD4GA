987186f507c062d11c55b555017f485cdbcb7192
jiujiuchen <jiujiuchen@tencent.com>
2025-01-26 16:00:12 +0800

    commit from my notebook

--- pytorch/trainer/uin_gangs_model_pretrain_run.py
+++ pytorch/trainer/uin_gangs_model_pretrain_run.py
@@ -63,9 +63,9 @@ class ArgsUinGangs:
         parser.add_argument('--re_train', type=bool, default=False)
         parser.add_argument('--is_debug', type=bool, default=True)
         parser.add_argument('--start_epoch', type=int, default=0)
-        parser.add_argument('--conv_type', type=str, default='HAN',
+        parser.add_argument('--conv_type', type=str, default='HGT',
                             choices=['RGCN', 'RGAT', 'HAN', 'HGT'])
-        parser.add_argument('--task_type', type=str, default='fine_grained_batch_subgraph',
+        parser.add_argument('--task_type', type=str, default='fine_grained_cross_subgraph',
                             choices=['subgraph', 'node_subgraph', 'batch_subgraph', 'cross_subgraph',
                                      'fine_grained_cross_subgraph', 'fine_grained_batch_subgraph'])
         # RGCN
