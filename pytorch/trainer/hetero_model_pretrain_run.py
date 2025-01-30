50a2127ef932e01a066c26678c284addc5767cc4
jiujiuchen <jiujiuchen@tencent.com>
2025-01-30 12:30:28 +0800

    commit from my notebook

--- pytorch/trainer/hetero_model_pretrain_run.py
+++ pytorch/trainer/hetero_model_pretrain_run.py
@@ -30,9 +30,9 @@ class ArgsHeteroGangs:
             os.path.join(os.path.dirname(__file__), os.path.pardir, "data", "config", "requirements" + ".txt")))
         parser.add_argument('--log_dir', type=str, default=os.path.abspath(
             os.path.join(os.path.dirname(__file__), os.path.pardir, os.path.pardir, "data", "losses")))
-        parser.add_argument('--best_loss', type=float, default=10)
-        parser.add_argument('--batch_size', type=int, default=32)
-        parser.add_argument('--lr', type=float, default=0.001)
+        parser.add_argument('--best_loss', type=float, default=1e2)
+        parser.add_argument('--batch_size', type=int, default=64)
+        parser.add_argument('--lr', type=float, default=1e-4)
         parser.add_argument('--n_epochs', type=int, default=100)
         parser.add_argument('--seed', type=int, default=42)
         parser.add_argument('--num_workers', type=int, default=32)
@@ -51,7 +51,7 @@ class ArgsHeteroGangs:
         # RGCN
         parser.add_argument('--world_size', type=int, default=2)
         # Evaluation
-        parser.add_argument('--evaluate', type=bool, default=True)
+        parser.add_argument('--evaluate', type=bool, default=False)
         parser.add_argument('--evaluate_times', type=int, default=5)
         parser.add_argument('--evaluate_epoch', type=int, default=0)
         parser.add_argument('--node_classes', type=int, default=5)
--- pytorch/trainer/uin_gangs_model_pretrain_run.py
+++ pytorch/trainer/uin_gangs_model_pretrain_run.py
@@ -37,11 +37,15 @@ class ArgsUinGangs:
             os.path.join(os.path.dirname(__file__), os.path.pardir, os.path.pardir, "data", "pic")))
         parser.add_argument('--minirbt_path', type=str, default=os.path.abspath(
             os.path.join(os.path.dirname(__file__), os.path.pardir, os.path.pardir, "minirbt-h256")))
-        parser.add_argument('--best_loss', type=float, default=3)
+        parser.add_argument('--best_loss', type=float, default=1e2)
         parser.add_argument('--negative_positive_ratio', type=float, default=10)
         parser.add_argument('--batch_size', type=int, default=40)
         parser.add_argument('--data_buffer_size', type=int, default=40)
-        parser.add_argument('--lr', type=float, default=0.001)
+        parser.add_argument('--lr', type=float, default=0.01)
+        parser.add_argument('--lr_scheduler', type=str, default='stepLR',
+                            choices=[None, 'stepLR', 'reduceLR', 'cosineLR'])
+        parser.add_argument('--lr_adjust_step', type=int, default=5)
+        parser.add_argument('--lr_gamma', type=float, default=0.5)
         parser.add_argument('--n_epochs', type=int, default=100)
         parser.add_argument('--uin_in_size', type=int, default=846)
         parser.add_argument('--uin_acs_numberical_feat_dim', type=int, default=290)
@@ -61,9 +65,9 @@ class ArgsUinGangs:
         parser.add_argument('--sampling', type=str, default='fraudar', choices=['fraudar', 'random'])
         parser.add_argument('--drop_ratio', type=float, default=0.2)
         parser.add_argument('--re_train', type=bool, default=False)
-        parser.add_argument('--is_debug', type=bool, default=True)
+        parser.add_argument('--is_debug', type=bool, default=False)
         parser.add_argument('--start_epoch', type=int, default=0)
-        parser.add_argument('--conv_type', type=str, default='HGT',
+        parser.add_argument('--conv_type', type=str, default='HAN',
                             choices=['RGCN', 'RGAT', 'HAN', 'HGT'])
         parser.add_argument('--task_type', type=str, default='fine_grained_cross_subgraph',
                             choices=['subgraph', 'node_subgraph', 'batch_subgraph', 'cross_subgraph',
